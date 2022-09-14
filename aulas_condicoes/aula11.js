f1a90da7ce7b96050411196d74dff1253481ebfb
maiconrmonteiro <maiconrmonteiro@gmail.com>
2022-09-13 23:04:08 -0300

    ultimas

---
+++ aulas_condicoes/aula11.js
@@ -0,0 +1,9 @@
+var velocidade = 50;
+
+if ( velocidade > 60){
+    console.log(`você passou a ${velocidade} KM foi multado, o limite é 50KM`);
+}else {
+    console.log(`Parabens, você está na velocidade correta`);
+}
+
+
